3ec9b4760662f1ac9b20fec4c8724a58e0436cb3
AlinaKuzmenko <alinakuzmenko@Alinas-MacBook-Pro.local>
2019-10-08 14:34:06 +0300

    add files

---
+++ index.js
@@ -0,0 +1,56 @@
+// const http = require('http');
+//
+// const hostname = '127.0.0.1';
+// const port = '3000';
+//
+// const server = http.createServer((req, res) => {
+//   res.statusCode = 200;
+//   res.setHeader('Content-Type', 'text/plain');
+//   res.end('Hello World\n');
+// });
+//
+// server.listen(port, hostname, () => {
+//   console.log(`Server running at host http://${hostname}:${port}`);
+// });
+
+// Frameworks: Express, Koa, Sailes (жесткая структура файлов), Action Hero
+
+// const express = require('express');
+// const app = express();
+// const port = 3000;
+//
+// app.get('/', (req, res) => res.send('Hello World!'));
+// app.get('/test', (req, res) => res.send('Hello TEST!'));
+//
+// app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+
+const Koa = require('koa');
+const Router = require('koa-router');
+
+const app = new Koa();
+const router = new Router();
+
+router
+  .post('/signup', async (ctx) => {
+    let arr = [];
+
+    await new Promise((resolve) => {
+      ctx.req.on('data', (data) => {
+        arr.push(data);
+      });
+
+      ctx.req.on('end', () => {
+        console.log(arr);
+        console.log(JSON.parse(arr));
+        ctx.body = JSON.parse(arr);
+        resolve();
+      })
+    })
+
+  });
+
+app
+  .use(router.routes())
+  .use(router.allowedMethods());
+
+app.listen(3000);
